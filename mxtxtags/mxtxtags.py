bddf6cd0a97eaf89ca7bf4b9f5302d696c32ed84
Gai <alicebeckam12345@gmail.com>
2020-04-10 22:37:29 +0800

    more style changes
    removed extra spaces around equals sign in parameters and keywords

--- mxtxtags/mxtxtags.py
+++ mxtxtags/mxtxtags.py
@@ -19,9 +19,9 @@ class Mxtxtags(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=79797979795641, force_registration=True)
         
-        tag_fp=bundled_data_path(self) / "taglist.json"
+        tag_fp = bundled_data_path(self) / "taglist.json"
         with tag_fp.open("r", encoding='utf-8') as tagses:
-            self.TAGLIST=json.load(tagses)
+            self.TAGLIST = json.load(tagses)
 
     @commands.command(aliases=['mxtxtags','tag', 'cntag'])
     async def mxtxtag(self, ctx, *, tags):
@@ -45,7 +45,7 @@ will return
         return
     
     @commands.command(aliases=['pairgen','mxtxpair'])
-    async def pairgenerator(self, ctx, fandom:str = "all", numpairs:int = 2):
+    async def pairgenerator(self, ctx, fandom:str="all", numpairs:int=2):
         """Generate a random MXTX pairing! 
 
 To generate less or more a pair, just include a number from 1 to 5. 
@@ -72,7 +72,7 @@ To limit to a fandom, just add ONLY one of the fandom flags: `mdzs` | `sv` | `tg
         return
 
     @commands.command(aliases=['chargen','mxtxchar'])
-    async def chargenerator(self, ctx, fandom:str = "all", numchars:int = 1):
+    async def chargenerator(self, ctx, fandom:str="all", numchars:int=1):
         """Generate a random MXTX character! 
 
 You can generate at most 5 characters. 
